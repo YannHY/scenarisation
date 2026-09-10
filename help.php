1abde6e87c36ac5a8d16315c61da5a8554967a17
YannHY <yannhoury@ralentirtravaux.com>
2026-09-10 15:45:52 +0200

    Fix help table of contents chevron alignment

--- help.php
+++ help.php
@@ -462,7 +462,7 @@ PROMPT;
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" referrerpolicy="no-referrer">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="css/interface.css?v=20260905-subtle-focus">
+    <link rel="stylesheet" href="css/interface.css?v=20260910-help-toc">
     <link rel="stylesheet" href="css/account-ui.css?v=20260906-highlight">
     <link rel="stylesheet" href="css/account-pages.css?v=20260904-content-rhythm">
 </head>
